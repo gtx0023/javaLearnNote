cc8efa361a3238a637c4592fce66d66fa816b52d
ccl <592796667@qq.com>
2022-04-06 00:35:13 +0800

    [description] proxy 代理模拟 泛型传入

--- examination/src/proxy/ProxyUnit.java
+++ examination/src/proxy/ProxyUnit.java
@@ -6,23 +6,21 @@ import java.lang.reflect.Proxy;
 
 public class ProxyUnit {
     /**
-     *
      * @param obj
      * @return
      */
-    public static UserService getProxy(UserService obj) {
+    public static <T> T getProxy(T obj) {
         /**
          * ClassLoader loader, 类加载器，负责加载代理类到内存中使用
          * Class<?>[] interfaces, 获取被代理对象实现的全部接口，代理要为全部接口的全部方法进行代理
          * InvocationHandler h， 代理的核心处理逻辑
          */
-        return (UserService)Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), new InvocationHandler() {
+        return (T) Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), new InvocationHandler() {
             /**
              *
              * @param proxy 代理对象本身，一般不管
              * @param method 正在被代理的方法
              * @param args 被代理方法，传入的参数
-             * @return
              * @throws Throwable
              */
             @Override
